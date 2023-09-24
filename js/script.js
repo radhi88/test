0bfb0b9a53f4e395f8813cb280cfff9d7a31048c
radhi88 <Radhi-ksa@hotmail.com>
2023-09-24 17:10:21 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
+const navbarLinks = document.querySelectorAll("#navbar a");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset > (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navbarLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").substr(1) === current) {
+            link.classList.add("active");
+        }
+    });
+});
+
 const ctaButton = document.querySelector("#cta button");
 ctaButton.addEventListener("click", function() {
     const contactSection = document.getElementById("contact");
     contactSection.scrollIntoView({ behavior: "smooth" });
 });
-
-const contactForm = document.querySelector("#contact form");
-contactForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for reaching out! We'll get
